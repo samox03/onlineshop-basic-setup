f11e156b81fc7615ca1e3933406616084dcbf489
samox_ <samirasteinmayer@web.de>
2022-02-08 22:21:54 +0100

    basic structure setup plus

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App headerText="Welcome to the Shop" extraText='Hello <username>' />
   </React.StrictMode>,
   document.getElementById('root')
 );
