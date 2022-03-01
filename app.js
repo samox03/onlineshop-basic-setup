c41104781586d3a1358da4cec35e096622e5dfdc
samox_ <samirasteinmayer@web.de>
2022-03-01 13:37:21 +0100

    Edited app.js with express configuration

--- app.js
+++ app.js
@@ -46,10 +46,46 @@ Store: MongoStore.create({mongoUrl: "mongodb://localhost/lab-profile-app"})
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+
+// allow access to the API from different domains/origins
+// app.use(cors({
+//   // this could be multiple domains/origins, but we will allow just our React app
+//   origin: [ "http://localhost:3000" ]
+// }));
+
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+app.locals.title = 'Onlineshop - A Prototype';
+
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
